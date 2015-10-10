832ff77a3cd41e57f7d389f24bf5a211c685fda2
Phantom1771 <justin.schiller@colorado.edu>
2015-10-10 13:38:21 -0600

    update

--- testPopup.js
+++ testPopup.js
@@ -42,4 +42,14 @@ function getCurrentTabUrl(callback) {
 function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
+function show_image(src, width, height, alt) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+
+    // This next line will just add it to the <body> tag
+    document.body.appendChild(img);
+}
 alert("Stop Slacking OFF!!!!")
