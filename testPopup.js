d57be9b222226f7cd915c622fcf6757395deed48
Jessica Petty <jessicapetty@rgnt1-19-78-dhcp.int.colorado.edu>
2015-10-10 13:53:02 -0600

    Add forbidden list

--- testPopup.js
+++ testPopup.js
@@ -42,6 +42,7 @@ function getCurrentTabUrl(callback) {
  */
 
 function compareURL() {
+  var forbiddenList = ["facebook", "twitter", "iwastesomuchtime", "reddit", "tumblr", "buzzfeed"]
   if (window.location.href.indexOf("facebook") > -1) {
   alert("You have tried to access a forbidden site. You are now being redirected to a Judgy Rabbit. Stop slacking OFF!");
   window.location=("http://1.bp.blogspot.com/-AKSZfnEBqrg/U1Cb_fe0jTI/AAAAAAAADmM/8giL1Zx0gBE/s1600/Lop-Eared-Pet-Rabbit.jpg")
